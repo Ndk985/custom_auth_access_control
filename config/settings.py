3d7c0ccc29a6d32af872e54198d30a1f71c9854e
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2026-01-05 17:55:04 +0300

    feat: configure DRF pagination and renderer classes

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.PageNumberPagination'
+    ),
+    'PAGE_SIZE': 20,
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
 }
 
 BASE_DIR = Path(__file__).resolve().parent.parent
